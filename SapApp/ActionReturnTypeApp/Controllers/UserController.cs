a0c81148e11256ddcad537c99b35f1f2e7a27436
alex-toy <alexei.80@hotmail.fr>
2023-07-12 20:32:38 +0200

    user controler

---
+++ SapApp/ActionReturnTypeApp/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Spa.Business.Services;
+using System.Collections.Generic;
+
+namespace ActionReturnTypeApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly IProductService _productService;
+
+        public UserController(ILogger<UserController> logger, IProductService weatherService)
+        {
+            _logger = logger;
+            _productService = weatherService;
+        }
+
+        [HttpGet("products")]
+        public IEnumerable<Product> Get(string prefix = "")
+        {
+            IEnumerable<Product> products = _productService.Get(prefix);
+            return products;
+        }
+    }
+}
